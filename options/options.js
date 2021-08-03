3b509059c382db668e277fd3f8e8a28037413674
Crowdbotics <team@crowdbotics.com>
2021-08-03 21:29:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "postey_29429Identifier",
+  url: "https://postey_29429Identifier.botics.co",
+  api: "https://postey_29429Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
